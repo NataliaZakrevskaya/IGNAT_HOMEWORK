c8b91413b8898bd5eecbd92d92a6bbbb6a594dcc
Natalia <zakrevskaya.97@inbox.ru>
2022-02-12 19:29:50 +0300

    5 homework is done.

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import {NavLink} from "react-router-dom";
+import {PATH} from "./RoutesComponent";
+import s from "./Header.module.css"
 
 function Header() {
     return (
-        <div>
-            <NavLink to={'/pre-junior'}>Pre-junior</NavLink>
-            <NavLink to={'/junior'}>Junior</NavLink>
-            <NavLink to={'/junior-plus'}>Pre-junior</NavLink>
+        <div className={s.navLinkBlock}>
+            <NavLink to={PATH.PRE_JUNIOR}><span className={s.links}>Pre-junior</span></NavLink>
+            <NavLink to={PATH.JUNIOR}><span className={s.links}>Junior</span></NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS}><span className={s.links}>Junior-plus</span></NavLink>
+            <span>Links</span>
         </div>
     )
 }
--- src/p2-homeworks/h5/RoutesComponent.tsx
+++ src/p2-homeworks/h5/RoutesComponent.tsx
@@ -24,13 +24,13 @@ function RoutesComponent() {
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
                 {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
                 {/*//@ts-ignore*/}
-                <Route path={'/'} element={() => <Navigate to={PATH.PRE_JUNIOR}/>}/>
+                <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
 
-                <Route path={PATH.PRE_JUNIOR} element={() => <PreJunior/>}/>
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
 
                 // add routes
-                <Route path={PATH.JUNIOR} element={() => <Junior/>}/>
-                <Route path={PATH.JUNIOR_PLUS} element={() => <JuniorPlus/>}/>
+                <Route path={PATH.JUNIOR} element={<Junior/>}/>
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
 
                 {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
                 <Route element={() => <Error404/>}/>
