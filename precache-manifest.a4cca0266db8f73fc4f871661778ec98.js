a650d7a1e4e5a6dd9c7573cd6ca453a263c9fa92
Natalia <zakrevskaya.97@inbox.ru>
2022-02-18 13:30:07 +0300

    Updates

---
+++ precache-manifest.a4cca0266db8f73fc4f871661778ec98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6277701f083034e78cdb6c51279d86ad",
+    "url": "/IGNAT_HOMEWORK/index.html"
+  },
+  {
+    "revision": "06dc0959a5c778c18665",
+    "url": "/IGNAT_HOMEWORK/static/css/main.7b09f999.chunk.css"
+  },
+  {
+    "revision": "85fa6c146880e1a6fe4e",
+    "url": "/IGNAT_HOMEWORK/static/js/2.32d6a2ea.chunk.js"
+  },
+  {
+    "revision": "f9149ae0f0977793c838a92fcf8ae78b",
+    "url": "/IGNAT_HOMEWORK/static/js/2.32d6a2ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06dc0959a5c778c18665",
+    "url": "/IGNAT_HOMEWORK/static/js/main.75a7f8c4.chunk.js"
+  },
+  {
+    "revision": "42cb7b293be0152410b0",
+    "url": "/IGNAT_HOMEWORK/static/js/runtime-main.61983498.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/IGNAT_HOMEWORK/precache-manifest.7522175f416976178f81c73627b41253.js"
+  "/IGNAT_HOMEWORK/precache-manifest.a4cca0266db8f73fc4f871661778ec98.js"
 );
 
 self.addEventListener('message', (event) => {
