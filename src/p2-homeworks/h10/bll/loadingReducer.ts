0010681952adcd7395f16ef31e08f07f6aae4eec
Natalia <zakrevskaya.97@inbox.ru>
2022-02-18 14:09:40 +0300

    10 homework is done.

--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,23 @@
 const initState = {
-
+isLoading: false as boolean
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+type InitStateType = typeof initState;
+
+
+
+export const loadingReducer = (state: InitStateType = initState, action: LoadingReducerType): InitStateType => {
     switch (action.type) {
-        case '': {
-            return state
+        case 'SET-LOADING': {
+            return {...state, isLoading: action.isLoading}
         }
         default: return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+export const loadingAC = (isLoading: boolean) => {
+    return {type: 'SET-LOADING', isLoading} as const
+}
+
+export type LoadingReducerType = loadingACType
+export type loadingACType = ReturnType<typeof loadingAC>
